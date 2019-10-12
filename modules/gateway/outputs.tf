9b401632cf5bfb3b1c16dd8548bf2f8775ffcd0b
lufussel <luke.fussell@microsoft.com>
2019-10-12 21:52:22 +0100

    Corrected output values

--- modules/gateway/outputs.tf
+++ modules/gateway/outputs.tf
@@ -1,39 +1,39 @@
 output "gateway_id" {
   description = "The id of the virtual network gateway."
-  value       = "${azurerm_firewall.firewall.id}"
+  value       = "${azurerm_virtual_network_gateway.gateway.id}"
 }
 
 output "gateway_name" {
   description = "The name of the virtual network gateway."
-  value       = "${azurerm_firewall.firewall.id}"
+  value       = "${azurerm_virtual_network_gateway.gateway.name}"
 }
 
 output "resource_group_name" {
   description = "The name of the resource group in which the virtual network gateway was created."
-  value       = "${azurerm_virtual_network.vnet.resource_group_name}"
+  value       = "${azurerm_virtual_network_gateway.gateway.resource_group_name}"
 }
 
 output "gateway_location" {
   description = "The location of the virtual network gateway."
-  value       = "${azurerm_firewall.firewall.location}"
+  value       = "${azurerm_virtual_network_gateway.gateway.location}"
 }
 
 output "vnet_id" {
   description = "The id of the virtual network."
-  value       = "${azurerm_virtual_network.vnet.id}"
+  value       = "${azurerm_virtual_network.gateway.id}"
 }
 
 output "vnet_name" {
   description = "The name of the virtual network."
-  value       = "${azurerm_virtual_network.vnet.name}"
+  value       = "${azurerm_virtual_network.gateway.name}"
 }
 
 output "gateway_subnet" {
   description = "The id of subnet inside the virtual network."
-  value       = "${azurerm_subnet.firewall.id}"
+  value       = "${azurerm_subnet.gateway.id}"
 }
 
 output "gateway_public_ip_id" {
   description = "The id of the public IP address of the gateway."
-  value       = "${azurerm_public_ip.firewall.id}"
+  value       = "${azurerm_public_ip.gateway.id}"
 }
