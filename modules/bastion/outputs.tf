b726721520258e984feb577c8eafac5660aa4e92
lufussel <luke.fussell@microsoft.com>
2019-10-20 19:48:02 +0100

    Corrected output reference

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -1,9 +1,9 @@
 output "bastion_subnet" {
   description = "The id of subnet inside the virtual network."
-  value       = "${azurerm_subnet.firewall.id}"
+  value       = "${azurerm_subnet.bastion.id}"
 }
 
 output "bastion_public_ip_id" {
   description = "The id of the public IP address of bastion."
-  value       = "${azurerm_public_ip.firewall.id}"
+  value       = "${azurerm_public_ip.bastion.id}"
 }
