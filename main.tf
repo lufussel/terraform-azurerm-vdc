ef81982514c2ee109340720825997b43f76d897e
Luke Fussell <44374451+lufussel@users.noreply.github.com>
2020-05-22 10:13:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ terraform {
   required_version          = ">= 0.12"
 }
 
+provider "azurerm" {
+  version = "= 2.0.0"
+  features {}
+}
+
 module "hub_network" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
